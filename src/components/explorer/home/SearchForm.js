e0abd3f54aebdc10ee0bf2faa5010702d01cc529
Rahul Bhargava <rahulbot@gmail.com>
2022-07-13 11:44:13 -0400

    remove non-working storycount on Explorer homepage

--- src/components/explorer/home/SearchForm.js
+++ src/components/explorer/home/SearchForm.js
@@ -13,29 +13,13 @@ const localMessages = {
   mainTitle: { id: 'explorer.search.title', defaultMessage: 'Enter Keyword' },
   addButton: { id: 'explorer.search', defaultMessage: 'Search' },
   searchHint: { id: 'explorer.intro.searchHint', defaultMessage: 'Try searching for the names of people, places, or events' },
-  search: { id: 'explorer.intro.search', defaultMessage: 'Search the Media Cloud database of over {storyCount} stories.' },
 };
 
 const SearchForm = (props) => {
-  const { handleSubmit, onSearch, renderTextField, storyCount } = props;
+  const { handleSubmit, onSearch, renderTextField } = props;
   // need to init initialValues a bit on the way in to make lower-level logic work right
   return (
     <form className="app-form search-form" name="searchForm" onSubmit={handleSubmit(onSearch.bind(this))}>
-      <Row>
-        <Col lg={3} />
-        <Col lg={6}>
-          {storyCount && (
-            <h2>
-              <FormattedMessage
-                {...localMessages.search}
-                values={{
-                  storyCount: humanReadableNumber(props.storyCount, 2, props.intl.formatNumber),
-                }}
-              />
-            </h2>
-          )}
-        </Col>
-      </Row>
       <Row>
         <Col lg={3} />
         <Col lg={6}>
@@ -56,7 +40,6 @@ SearchForm.propTypes = {
   // from parent
   onSearch: PropTypes.func.isRequired,
   initialValues: PropTypes.object,
-  storyCount: PropTypes.number,
   // from context
   intl: PropTypes.object.isRequired,
   renderTextField: PropTypes.func.isRequired,
